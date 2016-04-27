9f6ba9f56a91d784f4ed3e2afe23b0bcfaa5c637
lanleiming <lanleiming@126.com>
2016-04-27 13:24:06 +0800

    task20 完成

--- llm/2_20/task.js
+++ llm/2_20/task.js
@@ -4,47 +4,28 @@
 window.onload = function () {
     /* 数组队列 */
     var datalist = [];
-    var snapArray = [];
     var leftOrRight = {
         'left': 1,
         'right': 0
     };
-    /**
-     * 随机生成一组数据,填充到队列中
-     * 30个10到100的数字
-     */
-    function randomList(){
-        /**
-         * 随机生成一个指定范围内的数字
-         */
-        function RandomNum(Min,Max){
-            var Range = Max - Min;
-            var Rand = Math.random();
-            var num = Min + Math.round(Rand * Range);
-            return num;
-        }
-        datalist=[];
-       for(var i=0; i< 30;i++){
-           var number =RandomNum(10,100);
-           datalist.push(number);
-       }
-    }
 
     /**
      * 往队列中插入数据
      * 从左侧或者右侧插入
      */
     function insertIntoList(value, leftOrRightItem) {
-        if (value == '' || isNaN(value)|| parseInt(value) < 10 || parseInt(value) >100) {
-            alert('请输入正确的数字');
-            return;
-        }
-        if (leftOrRightItem == leftOrRight.left) {
-            datalist.splice(0, 0, value);
-        } else {
-            datalist.push(value);
+        var pattern = /[^0-9a-zA-Z\u4e00-\u9fa5]+/;
+        var arr = value.split(pattern);
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] == '' || arr[i] == null)continue;
+            if (leftOrRightItem == leftOrRight.left) {
+                datalist.splice(0, 0, arr[i]);
+            } else {
+                datalist.push(arr[i]);
+            }
         }
     }
+
 
     /**
      * 在队列中删除数据
@@ -62,58 +43,35 @@ window.onload = function () {
         }
     }
 
+
     /**
      * 渲染数组
      * 将数组中数据输出到id为data-list里面
      */
-    function render(arr) {
-        arr = arr || datalist;
+    function render(key) {
         var str = '';
-        arr.forEach(function (item, index) {
-            var c_color; //不同的高度，定义不同的颜色
-            if(item>80){
-                c_color = '#005db1;';
-            }else if(item>60){
-                c_color = '#73832a;';
-            }else if(item>40){
-                c_color = '#a02730;';
-            }else{
-                c_color = '#538289;';
+        datalist.forEach(function (item, index) {
+            if (key != null && key != '') {
+                item = item.replace(new RegExp('('+key+')','g'), '<span style="color:blue;">$1</span>');
             }
-            str += '<div class="data-item" style="background-color:'+c_color+' height:'+item*4+'px;" title="'+item+'" index="' + index + '"></div>';
+            str += '<p index="' + index + '">' + item + '</p>';
         });
         document.getElementById('data-list').innerHTML = str;
     }
 
     /**
-    * 冒泡排序
-    */
-    function bubble_sort(){
-        for(var i=0; i<datalist.length;i++){
-            for(var j=0; j<datalist.length-i-1 ;j++){
-                if (datalist[j] > datalist[j + 1]) {
-                    var temp = datalist[j+1];
-                    datalist[j+1] =datalist[j];
-                    datalist[j] = temp;
-                   // snapArray.push(datalist);
-                    snapArray.push(JSON.parse(JSON.stringify(datalist))); // 记录快照
-                }
-            }
+     * 查询
+     */
+    function search() {
+        var keyword = document.getElementById('txt_keyword').value;
+        if (keyword == null || keyword == '') {
+            alert('请输入要查询的值！');
+            return;
         }
-        var timer = setInterval(paint, 10); //定时绘制
-        function paint() {
-            var snapshot = snapArray.shift() || [];
-            if (snapshot.length !== 0) {
-                render(snapshot);
-            } else {
-                clearInterval(timer); //绘制结束
-            }
-        }
-
-
-
-
+        render(keyword);
     }
+
+
     /**
      * 点击 功能按钮的处理逻辑
      * 获取用户输入，更新数据，渲染数组
@@ -122,6 +80,7 @@ window.onload = function () {
         //处理逻辑
         //1、检测用户输入的是否为数字
         var inputValue = document.getElementById('txt_input').value;
+
         //2、根据点击的按钮类型，进行相应的处理
         switch (element.innerHTML) {
             case '左侧入':
@@ -131,21 +90,18 @@ window.onload = function () {
                 insertIntoList(inputValue, leftOrRight.right);
                 break;
             case '左侧出':
-                deleteValueFromList(inputValue, leftOrRight.left);
+                alert(deleteValueFromList(inputValue, leftOrRight.left));
                 break;
             case '右侧出':
-                deleteValueFromList(inputValue, leftOrRight.right);
+                alert(deleteValueFromList(inputValue, leftOrRight.right));
                 break;
-            case '随机产生':
-                randomList();
-
-                break;
-            case '冒泡排序':
-                bubble_sort();
-                break;
+            case '查询':
+                search();
+                return;
             default :
                 break;
         }
+        document.getElementById('txt_input').value = '';
         render();
     }
 
@@ -173,7 +129,7 @@ window.onload = function () {
         document.getElementById('data-list').addEventListener('click', function (event) {
             var e = event || window.event;
             var dom = e.srcElement || e.target;
-            if (dom.className.toLowerCase() == "data-item") {
+            if (dom.nodeName.toLowerCase() == "p") {
                 killMySelf(dom);
             }
         });
